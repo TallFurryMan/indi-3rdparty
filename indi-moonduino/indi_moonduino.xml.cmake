b5af42860c5e35ae38bd0d6d8d5ade1850373a8b
Eric Dejouhanet <eric.dejouhanet@gmail.com>
2022-07-12 13:31:41 +0200

    Moonduino, zero-based focuser/weather/dustcap.

---
+++ indi-moonduino/indi_moonduino.xml.cmake
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Focusers">
+    <device label="Moonduino Focuser" manufacturer="DIY">
+        <driver name="Moonduino Focuser">indi_moonduino</driver>
+        <version>@MOONDUINO_VERSION_MAJOR@.@MOONDUINO_VERSION_MINOR@</version>
+    </device>
+</devGroup>
+<devGroup group="Auxiliary">
+    <device label="Moonduino Dustcap" manufacturer="DIY">
+        <driver name="Moonduino Dustcap">indi_moonduino</driver>
+        <version>@MOONDUINO_VERSION_MAJOR@.@MOONDUINO_VERSION_MINOR@</version>
+    </device>
+</devGroup>
+<devGroup group="Weather">
+    <device label="Moonduino MeteoStation" manufacturer="DIY">
+        <driver name="Moonduino MeteoStation">indi_moonduino</driver>
+        <version>@MOONDUINO_VERSION_MAJOR@.@MOONDUINO_VERSION_MINOR@</version>
+    </device>
+</driversList>
